076189a1358df668af8c1e5a0edecd107e45bbd6
3A <524019351@qq.com>
2023-09-23 02:43:48 +0800

    Q1评估指标

--- q1.py
+++ q1.py
@@ -1,33 +1,30 @@
 allC = []
 
-for id in range(1, 121):
-    c = set()
-    for offset in range(5):
-        c.add(id + offset)
-    allC.append(c)
+def gen(k):
+    for id in range(1, 126):
+        c = set()
+        for offset in range(5):
+            val = id + offset + k
+            if val > 125:
+                val -= 125
+            c.add(val)
+        allC.append(c)
 
-# 补上以121-125开头的循环的情况
-for id in range(121, 126):
-    c = set()
-    for offset in range(5):
-        val = id + offset
-        if val > 125:
-            val -= 125
-        c.add(val)
-    allC.append(c)
+for k in range(24):
+    gen(k)
+
+print('num:', len(allC))
 
 def getCNum(id):
     num = 0
     coopId = []
     for c in allC:
         if id in c:
-            print(c)
+            # print(c)
             coopId += list(c)
             num += 1
-    print(set(coopId))
+    # print(set(coopId))
     return num
-
-getCNum(3)
 
 def getCNumTot(ids):
     num = 0
@@ -35,3 +32,12 @@ def getCNumTot(ids):
         num += getCNum(id)
     return num
 
+def calcTotIntersect(sub):
+    ids = allC[sub]
+    num = 0
+    for otherIds in allC:
+        num += len(otherIds & ids)
+    return num - 5
+
+print(calcTotIntersect(0))
+print(calcTotIntersect(49))
---
+++ q1Test.py
@@ -0,0 +1,40 @@
+import matplotlib.pylab as plt
+import numpy as np
+import random
+
+allC = []
+selectNum = {}
+for i in range(1, 126):
+    selectNum[i] = 0
+
+def randC():
+    c = set()
+    while len(c) < 5:
+        id = random.randint(1, 125)
+        c.add(id)
+        selectNum[id] += 1
+    return c
+
+for _ in range(3000):
+    allC.append(randC())
+
+def calcTotIntersect(sub):
+    ids = allC[sub]
+    num = 0
+    for otherIds in allC:
+        num += len(otherIds & ids)
+    return num - 5
+
+intNums = []
+for i in range(3000):
+    intNums.append(calcTotIntersect(i))
+
+print('平均数', np.mean(intNums))
+print('中位数', np.median(intNums))
+print('极差', max(intNums) - min(intNums))
+
+plt.ylim(60, 160)
+plt.bar(selectNum.keys(), selectNum.values(), )
+plt.show()
+plt.hist(intNums, bins=80)
+plt.show()
