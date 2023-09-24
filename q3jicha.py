95db8114ebfb6892cc08a60702cd0581a0b99c33
3A <524019351@qq.com>
2023-09-25 03:24:28 +0800

    截掉后的极差

--- q3jicha.py
+++ q3jicha.py
@@ -2,5 +2,11 @@ import pandas as pd
 import matplotlib.pylab as plt
 
 df = pd.read_csv('极差.csv')
-plt.hist(df['极差2'], bins=20)
+
+jicha = []
+for index, row in df.iterrows():
+    if row['一阶段成绩1']>39.9788 and row['一阶段成绩1']<58.5020:
+        jicha.append(row['极差1'])
+
+plt.hist(jicha, bins=20)
 plt.show()
