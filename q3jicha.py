43a82aa03545235552803dfbd472ffe066b1f860
3A <524019351@qq.com>
2023-09-25 11:45:45 +0800

    极差分析

--- q3jicha.py
+++ q3jicha.py
@@ -5,7 +5,7 @@ df = pd.read_csv('极差.csv')
 
 jicha = []
 for index, row in df.iterrows():
-    if row['一阶段成绩1']>39.9788 and row['一阶段成绩1']<58.5020:
+    if row['一阶段成绩1']>41.638 and row['一阶段成绩1']<58.3136:
         jicha.append(row['极差1'])
 
 plt.hist(jicha, bins=20)
